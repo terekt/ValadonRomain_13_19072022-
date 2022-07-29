db671c6dbce5cdf5e29c3a9e20b13076a49415e3
Romainv78 <romainvaladon@gmail.com>
2022-07-29 13:52:42 +0200

    structure

--- front-end/src/Services/api.js
+++ front-end/src/Services/api.js
@@ -1,27 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialStateValue = {firstName: "", lastName: ""};
-
 export const userSlice = createSlice({
     name: "user",
     initialState: {
-        value: initialStateValue,
-        connected: false,
-        token: ""
+        value: [],
     },
     reducers: {
-        login: (state, action) => {
+        createemployee: (state, action) => {
             state.value = action.payload[0];
-            state.connected = true;
-            state.token = action.payload[1];
-        },
-        logout: (state) => {
-            state.value = initialStateValue;
-            state.connected = false;
-            state.token = "";
-        },
-        editname: (state, action) => {
-            state.value.body = action.payload;
         }
     }
 })
